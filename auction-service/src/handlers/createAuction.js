d833294d8c60d201f1225e2daea0408ab44e71a0
Aek Sae-khow <saekhow.aek@gmail.com>
2020-07-03 15:15:48 +0700

    Done 30. CRUD Operation: Get Auctions

--- auction-service/src/handlers/createAuction.js
+++ auction-service/src/handlers/createAuction.js
@@ -1,11 +1,16 @@
 import {v4 as uuid} from 'uuid'
 import AWS from 'aws-sdk'
+import middy from '@middy/core'
+import httpErrorHandler from '@middy/http-error-handler'
+import httpEventNormalizer from '@middy/http-event-normalizer'
+import httpJsonBodyParser from '@middy/http-json-body-parser'
+import createError from 'http-errors'
 
 const dynamodb = new AWS.DynamoDB.DocumentClient()
 
 async function createAuction(event, context) {
 
-  const {title} = JSON.parse(event.body)
+  const {title} = event.body
   const now = new Date()
 
   const auction = {
@@ -15,17 +20,24 @@ async function createAuction(event, context) {
     createdAt: now.toISOString()
   }
 
-  await dynamodb.put({
-    TableName: 'AuctionsTable',
-    Item: auction
-  }).promise()
+  try {
+    await dynamodb.put({
+      TableName: process.env.AUCTIONS_TABLE_NAME,
+      Item: auction
+    }).promise()
+  }
+  catch (error) {
+    console.error(error)
+    throw new createError.InternalServerError(er)
+  }
 
   return {
     statusCode: 201,
     body: JSON.stringify(auction),
-  };
+  }
 }
 
-export const handler = createAuction;
-
-
+export const handler = middy(createAuction)
+                        .use(httpErrorHandler())
+                        .use(httpEventNormalizer())
+                        .use(httpJsonBodyParser())
