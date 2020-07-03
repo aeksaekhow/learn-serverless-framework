08f1578463a0e6b5acf36c5ed58816caf79fe7f0
Aek Sae-khow <saekhow.aek@gmail.com>
2020-07-03 22:00:37 +0700

    Done 38. Closing Auctions

--- auction-service/src/handlers/processAuctions.js
+++ auction-service/src/handlers/processAuctions.js
@@ -1,10 +1,21 @@
 import AWS from 'aws-sdk'
-import {getEndedAuctions} from '../libs/getEndedAuctions'
+import createError from 'http-errors'
+import { getEndedAuctions } from '../libs/getEndedAuctions'
+import { closeAuction } from '../libs/closeAuction'
 
 const dynamodb = new AWS.DynamoDB.DocumentClient()
 
 export const handler = async (event, context) => {
 
-    const endedAuctions = await getEndedAuctions()    
-    console.log('Ended auctions', endedAuctions)
+    try {
+        const endedAuctions = await getEndedAuctions()
+        const closeAuctionPromises = endedAuctions.map(auction => closeAuction(auction))
+        await Promise.all(closeAuctionPromises)
+
+        return {closed: closeAuctionPromises.length}
+    } 
+    catch (error) {
+        console.error(error)
+        throw new createError.InternalServerError(error)
+    }
 }
