38cb73f6694f4f0002ed1aad81d8306f9e9471a1
Aek Sae-khow <saekhow.aek@gmail.com>
2020-07-06 10:09:46 +0700

    40. (Challenge) Getting Auctions - Filter by Status

--- auction-service/src/handlers/getAuctions.js
+++ auction-service/src/handlers/getAuctions.js
@@ -1,17 +1,38 @@
 import AWS from 'aws-sdk'
-import {middyMiddleware} from '../libs/middyMiddleware'
+import { middyMiddleware } from '../libs/middyMiddleware'
 import createError from 'http-errors'
 
 const dynamodb = new AWS.DynamoDB.DocumentClient()
 
 export const handler = middyMiddleware(async (event, context) => {
 
+    let status;
+    if (event.queryStringParameters.status) {
+        status = event.queryStringParameters.status
+    }
     let auctions;
 
     try {
-        const result = await dynamodb.scan({
-            TableName: process.env.AUCTIONS_TABLE_NAME
-        }).promise()
+        let result;
+        if (status) {
+            result = await dynamodb.query({
+                TableName: process.env.AUCTIONS_TABLE_NAME,
+                IndexName: 'statusAndEndDate',
+                KeyConditionExpression: '#status = :status',
+                ExpressionAttributeValues: {
+                    ':status': status
+                },
+                ExpressionAttributeNames: {
+                    '#status': 'status'
+                }
+            }).promise()
+        }
+        else {
+            result = await dynamodb.scan({
+                TableName: process.env.AUCTIONS_TABLE_NAME
+            }).promise()
+        }
+
         auctions = result.Items
     } catch (error) {
         console.error(error)
